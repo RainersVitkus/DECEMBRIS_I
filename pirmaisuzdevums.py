880b14c21a4850faad26e9e33347b0f5b375c7c9
RainersVitkus <rainers.vitkus@grobinasskola.lv>
2023-11-23 11:41:04 +0200

    Pirmais uzdevums

---
+++ pirmaisuzdevums.py
@@ -0,0 +1,8 @@
+ievade=int(input("Ievadi skaitli:"))
+summa=0
+if ievade<=0:
+    print("Lūdzu ievadiet nenegatīvu skaitli :)")
+else:
+    while summa<ievade:
+        summa+=1
+        print(summa)
